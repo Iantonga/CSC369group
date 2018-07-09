55c0bc91bec23a04099d51d01d61de607e24a8d4
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2018-07-09 14:32:33 -0400

    that annoying bug was fixed;just needed to put () around HSIZE

--- a2/starter/opt.c
+++ a2/starter/opt.c
@@ -100,7 +100,6 @@ void insert(addr_t vaddr) {
     // IF the index returned by hash equals to 0/NULL we put it there
     // If not we do probing (2 cases when probing we found a empty or we found
     // the vaddr)
-    // TODO: we could've used direct hashing but for now we use that has functions and hope for the best that we don't do probing that much!!
     if (hash_table[hash(vaddr)] == NULL) {
         hash_table[hash(vaddr)] = (struct hash_tbl_entry*)malloc(1*sizeof(struct hash_tbl_entry));
         hash_table[hash(vaddr)]->vaddr = vaddr;
@@ -112,7 +111,7 @@ void insert(addr_t vaddr) {
         // PROBING (hash + i)
         int i;
         for (i = 0; i < HSIZE; i++) {
-            int index = (hash(vaddr) + i) % HSIZE;
+            int index = (hash(vaddr) + i) % (HSIZE);
             if (hash_table[index] != NULL && hash_table[index]->vaddr == vaddr) {
                 // printf("We are doing linkedlist operations for %lx!\n", vaddr);
                 append(index);
@@ -229,8 +228,7 @@ void opt_ref(pgtbl_entry_t *p) {
  */
 void opt_init() {
 	current_trace = 0;
-	// TODO:CAN WRITE A FUNCTION THAT GETS THE number of unique pages  or we could
-    // just use the SWAPSIZE if that's possible!
+	// TODO:CAN WRITE A FUNCTION THAT GETS THE number of unique pages!
 	// Initialize the hash table
 	for (int i = 0; i < HSIZE; i++) {
 		hash_table[i] = NULL;
