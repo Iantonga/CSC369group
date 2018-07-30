30414cb8960e7dd2e43decf9226b1518138725f5
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2018-07-30 11:56:10 -0400

    not sure what struct ext2_inode.i_blocks is

--- tut10/readimage.c
+++ tut10/readimage.c
@@ -39,16 +39,73 @@ int main(int argc, char **argv) {
     printf("    used_dirs: %d\n", gd->bg_used_dirs_count);
 
 
-    /* Task 2*/
+    /* Task 2.1: bitmap blocks*/
+    printf("Block bitmap: ");
     // The group descriptor often knows that blocks bitmap located at 3.
-    int bitmap_pos = gd->bg_block_bitmap;
-    
-    // printf("Block bitmap: ";)
-    // for (int i = 0; i < 16; i ++) {
-    //     for (int j = 0; j < 8; j++) {
-    //         printf("%d", );
-    //     }
-    // }
+    int bitmap_block_location = gd->bg_block_bitmap;
 
+    // Pointer to the beginning of the block bitmap (note: this is little
+    // endian;e.g. 0x80003fff is stored in mempry like ff3f0008).
+    unsigned char *bb_ptr = disk + EXT2_BLOCK_SIZE * bitmap_block_location;
+
+    // Since every bit in block bitmap corresponds to a data block, we need
+    // to get total number of blocks and divide that by 8 (1 byte) because
+    // we print space between each byte.
+    int total_blocks = sb->s_blocks_count;
+    for (int i = 0; i < total_blocks / 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            printf("%d", (*(bb_ptr + i) >> j) & 1);
+        }
+        printf(" ");
+    }
+
+    printf("\nInode bitmap: ");
+    int bitmap_inode_location = gd->bg_inode_bitmap;
+
+    unsigned char *bi_ptr = disk + EXT2_BLOCK_SIZE * bitmap_inode_location;
+
+    int total_inodes = sb->s_inodes_count;
+    for (int i = 0; i < total_inodes / 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            printf("%d", (*(bi_ptr + i) >> j) & 1);
+        }
+        printf(" ");
+    }
+
+    /* Task 2.2: inode table*/
+    // From ext2.h Inodes 1 to 10 are special inode numbers and first
+    // non-reserved inode for old ext2 filesystem is inode #11.
+    // printf("\n\nfirst nonres inode: %d, block group #: %d", sb->s_first_ino, sb->s_block_group_nr);
+    printf("\n\nInodes:\n");
+
+    int i_tbl_location = gd->bg_inode_table;
+    struct ext2_inode *it = (struct ext2_inode *)(disk + EXT2_BLOCK_SIZE * i_tbl_location);
+
+    for (int i = 0; i < total_inodes; i++) {
+        // If the inode is either root or none-reserved (do not print info
+        // about inode 11 at index 10 since it is the lost+found folder) and print
+        // those inodes that have size > 0!
+        char type = ' ';
+        int inode_num = i + 1;
+        if ((i == 1 || inode_num > EXT2_GOOD_OLD_FIRST_INO) && it[i].i_size > 0) {
+            if (it[i].i_mode & EXT2_S_IFREG) {
+                type = 'f';
+            } else if (it[i].i_mode & EXT2_S_IFDIR) {
+                type = 'd';
+            } else if (it[i].i_mode & EXT2_S_IFLNK) {
+                type = 'l';
+            }
+
+            printf("[%d] type: %c size: %d links: %d blocks: %d\n",
+                inode_num, type, it[i].i_size, it[i].i_links_count, it[i].i_blocks);
+            printf("[%d] Blocks:", inode_num);
+        }
+
+
+    }
+
+
+    printf("\n");
     return 0;
+
 }
