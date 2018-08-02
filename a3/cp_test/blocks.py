e91b05610662724b58c8ad87e89b0766fae67a11
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2018-08-02 00:15:48 -0400

    test cases for cp

---
+++ a3/cp_test/blocks.py
@@ -0,0 +1,12 @@
+def main():
+    # print("a" * 1022) # 1 block-- 1023
+    # print("a" * 1023) # 2 block-- 1024
+    print("y" * 1024) # 3 block-- 1025
+    #
+    # print("z" * 12288) # ...12 blocks
+    #
+    # print("z" * 274432) # first indirect
+
+
+if __name__ == '__main__':
+    main()
