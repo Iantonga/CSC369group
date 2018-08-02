e63bc38768c2bd58f8664d65c5c5a09fe2bbfaee
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2018-08-02 14:53:44 -0400

    updated the blocks.py that now takes an input

--- a3/cp_test/blocks.py
+++ a3/cp_test/blocks.py
@@ -1,12 +1,30 @@
-def main():
-    # print("a" * 1022) # 1 block-- 1023
-    # print("a" * 1023) # 2 block-- 1024
-    print("y" * 1024) # 3 block-- 1025
-    #
-    # print("z" * 12288) # ...12 blocks
-    #
-    # print("z" * 274432) # first indirect
+import sys
+
+BLOCK_SIZE = 1024
+
+def main(b):
+    if (b <= 0):
+        print "no_of_blocks should be greater than 0."
+        exit(1)
+
+    suffix = str(b)
+    file_name = "block"
+    if (b < 10):
+        file_name += "00" + suffix
+    elif (b < 100):
+        file_name += "0" + suffix
+    else:
+        file_name += suffix
+
+
+    size = b * BLOCK_SIZE - 2
+    f = open(file_name, "w")
+    print >>f,"s"+ ("z" * (size - 1)) + "e"
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 2:
+        print "Usage: python blocks.py <no_of_blocks>"
+        exit(1)
+
+    main(int(sys.argv[1]))
