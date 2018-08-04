4aef18cb48f914f52e6a6d5a7bf6225d40e24168
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2018-08-04 18:32:07 -0400

    updated imode back

--- a3/ext2_cp.c
+++ a3/ext2_cp.c
@@ -227,8 +227,8 @@ int copy_file(FILE *fsrc, struct ext2_inode* inode, struct ext2_super_block *sb,
 int create_inode(int pos, struct ext2_inode *inode_tbl, unsigned char *ib_ptr, FILE *fsrc, struct ext2_super_block *sb, struct ext2_group_desc *gd, unsigned char blk_map_ptr[]) {
     memset(&inode_tbl[pos], 0, sizeof(inode_tbl[pos])); // NOTE: problem may arise!
     inode_tbl[pos].i_mode |= 0x8000;
-    inode_tbl[pos].i_mode |= 0x002;
-    inode_tbl[pos].i_mode |= 0x004;
+    // inode_tbl[pos].i_mode |= 0x002;
+    // inode_tbl[pos].i_mode |= 0x004;
     inode_tbl[pos].i_atime = (unsigned int)time(NULL);
     inode_tbl[pos].i_ctime = (unsigned int)time(NULL);
     inode_tbl[pos].i_mtime = (unsigned int)time(NULL);
